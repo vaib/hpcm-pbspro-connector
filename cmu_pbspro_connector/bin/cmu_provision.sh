7abd70de66cd822f9ec13ff98c62aba4d263bc68
pradeeparmugam <52303152+pradeeparmugam@users.noreply.github.com>
2019-07-04 12:48:42 +0530

    HPE Contribution to Altair PBS for HPCM (#7)

--- cmu_pbspro_connector/bin/cmu_provision.sh
+++ cmu_pbspro_connector/bin/cmu_provision.sh
@@ -5,9 +5,9 @@
 # indemnification of any kind.  All other terms and conditions are as
 # specified in the Altair PBS EULA.
 
-# HP CMU Logical Groups: A logical group in HP Insight CMU represents a disk
+# HPCM Image Groups: A image group in HPCM represents a disk
 #       image that has been captured (backed up). Based on this definition the
-#       logical group will be associated to the PBS Professional application
+#       image group will be associated to the PBS Professional application
 #       operating environment (aoe) native resource.
 
 
@@ -16,8 +16,8 @@ PID=$$
 ERR=255
 
 # Define CMU environment and construct absolute PATH for commands
-CMU_PATH=/opt/cmu
-CMU_SHOW_LOGICAL_GROUPS=${CMU_PATH}/bin/cmu_show_logical_groups
+CMU_PATH=/opt/clmgr
+CMU_SHOW_LOGICAL_GROUPS=${CMU_PATH}/bin/cmu_show_image_groups
 CMU_SHOW_NODES=${CMU_PATH}/bin/cmu_show_nodes
 CMU_CLONE=${CMU_PATH}/bin/cmu_clone
 
@@ -37,11 +37,11 @@ function myecho
 }
 
 # Check arguments
-# 1. CMU logical group (aka PBS aoe)
-# 2. valid CMU client nodes
+# 1. HPCM image group (aka PBS aoe)
+# 2. valid HPCM client nodes
 
 if [[ $# < 2 ]]; then
-    myecho "usage: $0 <CMU logical group> <CMU node to clone>"
+    myecho "usage: $0 <HPCM image group> <HPCM node to clone>"
     exit 1
 fi
 
@@ -50,31 +50,31 @@ myecho "starting: $0 $*"
 cmu_lg=$1
 node=$2
 
-# Reject provisioning the CMU Server, which is also running PBS Professional Server/Scheduler
+# Reject provisioning the HPCM Server, which is also running PBS Professional Server/Scheduler
 hostname=`hostname`
 if [ "$node" = "$hostname" ]; then
     myecho "Cannot reboot own machine. Please provide another machine name"
     exit $ERR
 fi
 
-# Verify Logical Group (aoe) exists
+# Verify Image Group (aoe) exists
 vnodes=`${CMU_SHOW_LOGICAL_GROUPS} ${cmu_lg}`
 if [ $? -ne 0 ]; then
-    myecho "${cmu_lg} is not a valid Logical Group (aoe)"
+    myecho "${cmu_lg} is not a valid Image Group (aoe)"
     exit $ERR
 fi
 
-# Verify node exists in Logical Group (aoe)
+# Verify node exists in Image Group (aoe)
 echo -e "${vnodes}" | grep "^${node}$"
 if [ $? -ne 0 ]; then
-    myecho "vnode ${node} is not associated to Logical Group (aoe)"
+    myecho "vnode ${node} is not associated to Image Group (aoe)"
     exit $ERR
 fi
 
-# Check whether node is already running the logical group (aoe)
+# Check whether node is already running the image group (aoe)
 current_lg=`${CMU_SHOW_NODES} -o "%l" -n ${node}`
 if [ "${current_image}" = "${cmu_lg}" ]; then
-    myecho "Logical Group (aoe) already running on ${node}."
+    myecho "Image Group (aoe) already running on ${node}."
     exit 0
 fi
 
@@ -85,5 +85,5 @@ if [ $? -ne 0 ]; then
     exit $ERR
 fi
 
-myecho "${node} is now up and running with Logical Group (aoe) ${cmu_lg}"
+myecho "${node} is now up and running with Image Group (aoe) ${cmu_lg}"
 exit 0
