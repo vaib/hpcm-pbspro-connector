fb59b9fd96f69c413f40b85945db82807f327521
vaibhav saxena <vaib_z@yahoo.com>
2019-09-01 17:45:51 +1000

    fixes for os prov, HA server restart (#18)
    - os prov now requires kernel, prov type, fstype
    - restarting daemon means restarting primary and/or secondary
    - readme update with values to expect for fstype, prov type

--- hpcm_pbspro_connector/bin/hpcm_pbs_os_provision_hook.py
+++ hpcm_pbspro_connector/bin/hpcm_pbs_os_provision_hook.py
@@ -43,14 +43,46 @@ e = pbs.event()
 vnode = e.vnode
 aoe = e.aoe
 
+# event=provision runs only on server. user can request an aoe to be provisioned with a kernal of his choice on either disk or tmpfs and on a flat node, or ICE etc.
+# provisioning CLI in HPCM requires more than AOE and Vnode. It requires kernal, rootfs type and node type. Based on this info, right image is provisioned on right node. So user will have to supply  this additional information. Since support by PBS in this regard is not possible in limited time, a workaround is provided.
+
+# Provision hook will run on PBS Server but provisioning is started from admin node
+# Check for admin node? Read from json config file.
+
+kernel = os.environ["PBS_HPCM_KERNAL"]
+prov   = os.environ["PBS_HPCM_PROVTYPE"].lower()
+rootfs = os.environ["PBS_HPCM_ROOTFS"].lower()
+
 pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: Env = %s" % repr(os.environ))
 pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: PBS Node = %s" % vnode)
 pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: AOE = %s" % aoe)
+pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: PROVISION TYPE = %s" % prov)
+pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: KERNAL = %s" % kernal)
+pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: ROOTFS = %s" % rootfs)
 
-ret = os.system("/opt/clmgr/contrib/hpcm_pbspro_connector/bin/hpcm_provision.sh " + aoe + " " + vnode)
-if ret != 0:
-    pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: Failed - retcode = %s" % str(ret))
-    e.reject("Reboot provisioning failed", ret)
+if 'PBS_HOOK_CONFIG_FILE' in os.environ:
+    import json
+    config_file = os.environ["PBS_HOOK_CONFIG_FILE"]
+    #pbs.logmsg(pbs.EVENT_DEBUG, "%s: Config file is %s" % (caller_name(), config_file))
+    config = json.load(open(config_file, 'r'), object_hook=decode_dict)
+
+server = pbs.server().name
+admin = config['admin-node']
+
+pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: server name = %s" % server)
+pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: admin node = %s" % admin)
+
+if admin == server:
+    ret = os.system("/opt/clmgr/contrib/hpcm_pbspro_connector/bin/hpcm_provision.sh " + aoe + " " + vnode + " " + kernal + " " + prov + " " + rootfs)
+    if ret != 0:
+        pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: Failed - retcode = %s" % str(ret))
+        e.reject("Reboot provisioning failed", ret)
+    else:
+        e.accept(0)
 else:
-    os.system("/opt/clmgr/bin/cmu_power -p BOOT -n " + vnode)
-    e.accept(0)
+    ret = os.system("ssh ${admin} /opt/clmgr/contrib/hpcm_pbspro_connector/bin/hpcm_provision.sh " + aoe + " " + vnode + " " + kernal + " " + prov + " " + rootfs)
+    if ret != 0:
+        pbs.logmsg(pbs.LOG_DEBUG, "PROVISIONING: Failed - retcode = %s" % str(ret))
+        e.reject("Reboot provisioning failed", ret)
+    else:
+        e.accept(0)
